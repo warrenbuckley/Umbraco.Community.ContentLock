9fc62f9d0d62c59a82d776df028cd508eb4a9e91
Warren Buckley <warren@hackmakedo.com>
2025-03-17 17:36:02 +0000

    Remove the workspace view for now

--- ContentLock/Client/src/bundle.manifests.ts
+++ ContentLock/Client/src/bundle.manifests.ts
@@ -6,7 +6,7 @@ import { manifests as userPermissions } from './userpermissions/manifest';
 import { manifests as workspaceActions } from './workspaceActions/manifest';
 import { manifests as workspaceContexts } from './workspaceContexts/manifest';
 import { manifests as workspaceFooterApps } from './workspaceFooterApp/manifest';
-import { manifests as workspaceViews } from './workspaceViews/manifest';
+//import { manifests as workspaceViews } from './workspaceViews/manifest';
 
 // Job of the bundle is to collate all the manifests from different parts of the extension and load other manifests
 // We load this bundle from umbraco-package.json
@@ -19,5 +19,5 @@ export const manifests: Array<UmbExtensionManifest> = [
   ...workspaceActions,
   ...workspaceContexts,
   ...workspaceFooterApps,
-  ...workspaceViews,
+  //...workspaceViews, // TODO: Remnember to remove this if we are certain we no longer what this UX
 ];
