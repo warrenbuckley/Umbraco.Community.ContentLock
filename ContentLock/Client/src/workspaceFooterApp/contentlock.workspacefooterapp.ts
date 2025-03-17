44002124bb14c99a06d6fdb80b58475b80c0cd80
Warren Buckley <warren@hackmakedo.com>
2025-03-17 15:20:15 +0000

    Clean up of footer app

--- ContentLock/Client/src/workspaceFooterApp/contentlock.workspacefooterapp.ts
+++ ContentLock/Client/src/workspaceFooterApp/contentlock.workspacefooterapp.ts
@@ -1,16 +1,12 @@
 import { LitElement, css, customElement, html, nothing, state } from "@umbraco-cms/backoffice/external/lit";
 import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-
-import "@umbraco-cms/backoffice/external/uui";
-
 import { CONTENTLOCK_WORKSPACE_CONTEXT, ContentLockWorkspaceContext } from "../workspaceContexts/contentlock.workspace.context";
 import { observeMultiple } from "@umbraco-cms/backoffice/observable-api";
 import PageState from "../enums/PageStateEnum";
 
 @customElement('contentlock-workspacefooterapp')
 export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitElement) {
-
 
     @state()
 	private pageState: PageState;
@@ -36,16 +32,12 @@ export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitEl
 
             this._lockContext = lockCtx;
 
-
+            // Observe the many observables from lock context
+            // So this reacts when we move between locked states for future SignalR work
             this._lockContext?.observe(observeMultiple([this._lockContext?.isLocked, this._lockContext?.isLockedBySelf, this._lockContext?.lockedByName]), ([isLocked, isLockedBySelf, lockedByName]) => {
                 this._isLocked = isLocked;
                 this._isLockedBySelf = isLockedBySelf;
                 this._lockedByName = lockedByName;
-
-
-                console.log('isLocked', isLocked);
-                console.log('isLockedBySelf', isLockedBySelf);
-                console.log('lockedByName', lockedByName);
 
                 if (this._isLockedBySelf) {
                     this.pageState = PageState.LockedByYou;
@@ -56,8 +48,6 @@ export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitEl
                 else {
                     this.pageState = PageState.Unlocked;
                 }
-
-                console.log('Page State', this.pageState);
             });
         });
     }
@@ -71,7 +61,7 @@ export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitEl
             
             case PageState.LockedByYou:
                 return html `
-                    <div id="message" data-locked-by-you>
+                    <div id="message">
                         This page is locked by you
                         <uui-badge color="warning" look="primary" attention>
                             <uui-icon name="icon-lock"></uui-icon>
@@ -81,7 +71,7 @@ export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitEl
             
             case PageState.LockedByAnother:
                 return html `
-                    <div id="message" data-locked-by-another>
+                    <div id="message">
                         This Page is locked by ${this._lockedByName}
                         <uui-badge color="danger" look="primary" attention>
                             <uui-icon name="icon-lock"></uui-icon>
@@ -109,24 +99,9 @@ export class ContentLockWorkspaceVFooterAppElement extends UmbElementMixin(LitEl
                 font-size: 12px;
                 position:relative;
 
-                /* border: 1px solid var(--uui-color-surface);
-                color: var(--color-standalone);
-                border-color: var(--color-standalone); */
-
-                /* background-color: var(--uui-color-default);
-                color: var(--uui-color-default-contrast); */
-
                 background-color: var(--uui-color-surface-alt);
                 color: var(--uui-color-default-standalone);
                 font-weight: 700;
-            }
-
-            #message[data-locked-by-you] {
-               
-            }
-
-            #message[data-locked-by-another] {
-
             }
         `,
     ];
