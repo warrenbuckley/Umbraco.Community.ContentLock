59ce5cb121c2c2df404396f346d9777af40adb1e
Warren Buckley <warren@hackmakedo.com>
2025-03-18 18:39:11 +0000

    Remove the modal that we no longer use

--- ContentLock/Client/src/bundle.manifests.ts
+++ ContentLock/Client/src/bundle.manifests.ts
@@ -2,7 +2,6 @@ import { manifests as entrypoints } from './entrypoints/manifest';
 import { manifests as conditions } from './conditions/manifest';
 import { manifests as dashboards } from './dashboards/manifest';
 import { manifests as entityActions } from './entityActions/manifest';
-import { manifests as modals } from './modals/manifest';
 import { manifests as userPermissions } from './userpermissions/manifest';
 import { manifests as workspaceActions } from './workspaceActions/manifest';
 import { manifests as workspaceContexts } from './workspaceContexts/manifest';
@@ -16,7 +15,6 @@ export const manifests: Array<UmbExtensionManifest> = [
   ...conditions,
   ...dashboards,
   ...entityActions,
-  ...modals,
   ...userPermissions,
   ...workspaceActions,
   ...workspaceContexts,
