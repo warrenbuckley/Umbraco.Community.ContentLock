23ba89353299e14e73d8fc6dec5994c000ccf870
Warren Buckley <warren@hackmakedo.com>
2025-03-19 14:19:38 +0000

    Set the build value to 1.0.0

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 $projectFile = "./ContentLock/ContentLock.csproj"
 $configuration = "Release"
 $outputDirectory = "./build.out"
-$version = "2.0.0"
+$version = "1.0.0"
 
 
 ## Perhaps need to do a build of the client after updating the version in the source file
