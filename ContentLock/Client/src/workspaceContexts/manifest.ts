fb77f1c9dc2e796bfab517584312d45360edd8e4
Warren Buckley <warren@hackmakedo.com>
2025-03-24 01:10:58 +0000

    Add a TODO to investigate usage of GlobalContext

--- ContentLock/Client/src/workspaceContexts/manifest.ts
+++ ContentLock/Client/src/workspaceContexts/manifest.ts
@@ -1,6 +1,12 @@
 import { UMB_DOCUMENT_WORKSPACE_ALIAS } from '@umbraco-cms/backoffice/document';
 import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 
+// TODO [BUG]: Perhaps the entity actions for default actions such as save, publish, move
+// That are curently hiding in the tree actions but fine inside the document workspace/content node
+
+// TODO: Does it need to be a GLOBAL context instead that then 
+// keeps track of the unique node GUID and its state of isLocked and isLockedByYou perhaps ?
+
 export const manifests: Array<UmbExtensionManifest> = [
     {
         alias: 'ContentLock.WorkspaceContext',
