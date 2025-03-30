901c8046333037f59ba837ba431de108e64902d7
Owain Jones <owaingijones@outlook.com>
2025-03-30 23:35:50 +0100

    Add Welsh translations

---
+++ ContentLock/Client/src/localizations/cy.ts
@@ -0,0 +1,33 @@
+﻿export default {
+    contentLockDashboard: {
+        label: 'Clo Cynnwys',
+        pageNameHeader: 'Enw\'r Dudalen',
+        contentTypeHeader: 'Math o Gynnwys',
+        checkedOutByHeader: 'Wedi\'i Wirio Allan Gan',
+        checkedOutAtHeader: 'Wedi\'i Wirio Allan Ar',
+        lastEditedHeader: 'Golygiad Diwethaf',
+        unlockAction: 'Datgloi',
+        pagesCheckedOutTitle: 'Tudalennau wedi\'u Gwirio Allan',
+    },
+    contentLockFooterApp: {
+        lockedByYou: 'Mae\'r dudalen hon wedi\'i chloi gennych chi',
+        lockedByAnother: 'Mae\'r dudalen hon wedi\'i chloi gan {0}',
+    },
+    contentLockNotification: {
+        lockedHeader: 'Cynnwys wedi\'i Gloi',
+        lockedMessage: 'Mae\'r ddogfen wedi\'i chloi er mwyn i chi ei golygu.',
+        unlockedHeader: 'Cynnwys wedi\'i Ddatgloi',
+        unlockedMessage: 'Mae\'r ddogfen wedi\'i datgloi i ganiatáu i ddefnyddwyr eraill ei golygu.',
+        bulkUnlockHeader: 'Cynnwys wedi\'i Ddatgloi',
+        bulkUnlockMessage: 'Mae\'r cynnwys a ddewiswyd wedi\'i ddatgloi\'n llwyddiannus',
+    },
+    contentLockPermission: {
+        group: 'Clo Cynnwys', // TODO: Currently not used in Umbraco but added for future use
+        label: 'Datglöwr',
+        description: 'Yn caniatáu i\'r grŵp o ddefnyddwyr ddatgloi dogfen sydd wedi\'i chloi gan ddefnyddiwr arall.',
+    },
+    contentLockUsersModal: {
+        modalHeader: 'Pwy sy\'n ar-lein?',
+        listOfUsers: 'Defnyddwyr Ar-lein'
+    }
+};
