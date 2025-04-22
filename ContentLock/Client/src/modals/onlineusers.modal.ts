bba673fb18104449a3d72e9a902744b25810663b
Warren Buckley <warren@hackmakedo.com>
2025-04-22 22:07:43 +0100

    Uses the repository to get the info inside the modal
    Left some TODOs to revisit

--- ContentLock/Client/src/modals/onlineusers.modal.ts
+++ ContentLock/Client/src/modals/onlineusers.modal.ts
@@ -1,8 +1,9 @@
-import { css, customElement, html, state } from "@umbraco-cms/backoffice/external/lit";
+import { css, customElement, html, state, nothing } from "@umbraco-cms/backoffice/external/lit";
 import { UmbModalBaseElement, UmbModalRejectReason } from "@umbraco-cms/backoffice/modal";
 import { OnlineUsersModalData, OnlineUsersModalValue } from "./onlineusers.modal.token";
-import { UMB_USER_DETAIL_STORE_CONTEXT, UmbUserDetailRepository, UmbUserItemModel, UmbUserItemRepository } from "@umbraco-cms/backoffice/user";
+import { UmbUserItemModel, UmbUserItemRepository } from "@umbraco-cms/backoffice/user";
 import ContentLockSignalrContext, { CONTENTLOCK_SIGNALR_CONTEXT } from "../globalContexts/contentlock.signalr.context";
+import { UMB_CURRENT_USER_CONTEXT } from "@umbraco-cms/backoffice/current-user";
 
 @customElement("contentlock-onlineusers-modal")
 export class OnlineUsersModalElement extends UmbModalBaseElement<OnlineUsersModalData, OnlineUsersModalValue>
@@ -13,19 +14,37 @@ export class OnlineUsersModalElement extends UmbModalBaseElement<OnlineUsersModa
     @state()
     _connectedUserKeys: string[] = [];
 
+    @state()
+    _currentUserKey?: string;
+
     #userItemRepository = new UmbUserItemRepository(this);
 
     constructor() {
         super();
 
-        this.consumeContext(CONTENTLOCK_SIGNALR_CONTEXT, (signalrContext: ContentLockSignalrContext) => {
-            this.observe(signalrContext.connectedUserKeys, (connectedUserKeys) => {
-                console.log('KEYS from Global CTX in MODAL', connectedUserKeys);
+        // TODO: Make this a routable modal
+        // Can a modal have a condition so if setting is not enabled then we can route to
+
+        this.consumeContext(UMB_CURRENT_USER_CONTEXT, (currentUserCtx) => {
+            this.observe(currentUserCtx.unique, (unique) => {
+                this._currentUserKey = unique;
+            });
+        });
+
+        this.consumeContext(CONTENTLOCK_SIGNALR_CONTEXT, (signalrCtx: ContentLockSignalrContext) => {
+            // The list of GUID connected users as keys/uniques
+            this.observe(signalrCtx.connectedUserKeys, async (connectedUserKeys) => {
                 this._connectedUserKeys = connectedUserKeys;
 
-                // TODO use repository (Whats the difference between items and requestItems ?)
-                // this.#userItemRepository.items()
-                // this.#userItemRepository.requestItems('');
+                // Get users from the repo and observe it
+                // TODO: Why does it not work when a user updates their name or avatar?
+                // However when new user logs in or out it reactively shows them with the model open
+                const userItemsObservable = (await this.#userItemRepository.requestItems(connectedUserKeys)).asObservable();
+
+                // Observe the users we wanted to request/fetch and assign them to property/state
+                this.observe(userItemsObservable, (userItems) => {
+                    this._connectedUsersModels = userItems;
+                });
             });
         });
     }
@@ -39,10 +58,21 @@ export class OnlineUsersModalElement extends UmbModalBaseElement<OnlineUsersModa
             <umb-body-layout headline=${this.localize.term('contentLockUsersModal_modalHeader')}>
                 <uui-box headline=${this.localize.term('contentLockUsersModal_listOfUsers')}>
                     ${this._connectedUsersModels?.map((user) => {
-                        console.log('User to output to HTML', user);
+                        // Get the last item in the array of avatar URLs as this is the biggest or fallback to nothing
+                        const avatarUrl = user.avatarUrls?.[user.avatarUrls.length - 1] ?? '';
                         return html`
-                            <div>
-                                <uui-avatar name="${user.name}" img-src="${user.avatarUrls[0]}"></uui-avatar> ${user.name}
+                            <div class="user-detail">
+                                <uui-avatar name="${user.name}" img-src="${avatarUrl}">
+                                    <!-- TODO: This is to display how many SignalR connections the user has (aka tabs open etc) -->
+                                    <uui-badge color="default" look="primary">2</uui-badge>
+                                </uui-avatar>
+                                <span>${user.name}</span>
+                                
+                                <!-- Show a tag if the user is the current user -->
+                                ${user.unique === this._currentUserKey
+                                    ? html `<uui-tag color="default" look="outline">You</uui-tag>`
+                                    : nothing
+                                }
                             </div>
                         `;
                     })}
@@ -65,8 +95,19 @@ export class OnlineUsersModalElement extends UmbModalBaseElement<OnlineUsersModa
             padding-bottom:0;
         }
 
-        div {
-            margin: var(--uui-size-5) 0;
+        .user-detail {
+            display: flex;
+            align-items: center;
+            gap: var(--uui-size-4);
+            margin: var(--uui-size-3) 0;
+        }
+
+        .user-detail:last-child {
+            margin-bottom: 0;
+        }
+
+        uui-avatar {
+            font-size: var(--uui-size-6);
         }
     `;
 }
