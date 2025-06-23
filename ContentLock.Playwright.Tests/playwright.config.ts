2aee977244135849f9ba43d10643f6461def3d90
Warren Buckley <warren@hackmakedo.com>
2025-06-23 21:32:50 +0100

    Lets try on HTTP and see if its happy

--- ContentLock.Playwright.Tests/playwright.config.ts
+++ ContentLock.Playwright.Tests/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://localhost:5001',
+    baseURL: process.env.CI ? 'http://localhost:5000' : 'https://localhost:5001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -85,8 +85,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     cwd: path.join(__dirname, '../ContentLock.Website'),
-    command: 'dotnet run --urls "https://localhost:5001"',
-    url: 'https://localhost:5001',
+    command: 'dotnet run --urls "https://localhost:5001;http://localhost:5000"',
+    url: process.env.CI ? 'http://localhost:5000' : 'https://localhost:5001',
     stderr: 'pipe',
     stdout: 'pipe',
     ignoreHTTPSErrors: true,
