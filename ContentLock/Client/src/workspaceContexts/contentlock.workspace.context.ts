af6014fab50c597452afdb111477cd8fec99d67c
Warren Buckley <warren@hackmakedo.com>
2025-06-24 20:09:09 +0100

    Trying out readOnlyGuard

--- ContentLock/Client/src/workspaceContexts/contentlock.workspace.context.ts
+++ ContentLock/Client/src/workspaceContexts/contentlock.workspace.context.ts
@@ -1,18 +1,20 @@
 import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import { type UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UMB_DOCUMENT_WORKSPACE_CONTEXT, UmbDocumentWorkspaceContext } from '@umbraco-cms/backoffice/document';
+import { UMB_DOCUMENT_WORKSPACE_CONTEXT, UmbDocumentVariantModel, UmbDocumentWorkspaceContext } from '@umbraco-cms/backoffice/document';
 import { observeMultiple, UmbBooleanState, UmbStringState } from '@umbraco-cms/backoffice/observable-api';
 import { UmbEntityUnique } from '@umbraco-cms/backoffice/entity';
 import ContentLockSignalrContext, { CONTENTLOCK_SIGNALR_CONTEXT } from '../globalContexts/contentlock.signalr.context';
 import { UMB_CURRENT_USER_CONTEXT } from '@umbraco-cms/backoffice/current-user';
 import { UMB_CONFIRM_MODAL, umbOpenModal } from '@umbraco-cms/backoffice/modal';
 import { UmbLocalizationController } from '@umbraco-cms/backoffice/localization-api';
+import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 
 export class ContentLockWorkspaceContext extends UmbContextBase {
 
     #docWorkspaceCtx?: UmbDocumentWorkspaceContext;
     #unique: UmbEntityUnique | undefined;
+    #variants: UmbDocumentVariantModel[] = [];
   
     #isLocked = new UmbBooleanState(false);
     isLocked = this.#isLocked.asObservable();
@@ -47,8 +49,9 @@ export class ContentLockWorkspaceContext extends UmbContextBase {
         this.consumeContext(UMB_DOCUMENT_WORKSPACE_CONTEXT, (docWorkspaceCtx) => {
             this.#docWorkspaceCtx = docWorkspaceCtx;
 
-            this.#docWorkspaceCtx?.observe(this.#docWorkspaceCtx?.unique, (unique) => {
+            this.#docWorkspaceCtx?.observe(observeMultiple([this.#docWorkspaceCtx?.unique, this.#docWorkspaceCtx?.variants]), ([unique, variants]) => {
                 this.#unique = unique;
+                this.#variants = variants;
                 this.checkContentLockState();
             });
         });
@@ -86,10 +89,24 @@ export class ContentLockWorkspaceContext extends UmbContextBase {
                         permitted: false,
                         message: `This page is locked by ${lockInfo?.checkedOutBy}`
                     });
+
+                    // Set the read only state of the document for ALL culture & segment variant combinations
+                    // Even documents without a variant will have a default variant with the culture and segment set to null
+                    const rules = this.#variants.map(variant => ({
+                        unique: `${this.#unique!.toString()}-${variant.culture}`,
+                        variantId: new UmbVariantId(variant.culture, variant.segment),
+                        permitted: false,
+                        message: `This page is locked by ${lockInfo?.checkedOutBy}`
+                    }));
+                    this.#docWorkspaceCtx?.readOnlyGuard.addRules(rules);
                 }
                 else {
                     // Page is not locked or its locked by self - remove the readonly (propertyWriteGuard)
                     this.#docWorkspaceCtx?.propertyWriteGuard.removeRule(`ContentLock-${this.#unique!.toString()}`);
+
+                    // Same for the read only state of the document for ALL culture & segment variant combinations
+                    const ruleKeys = this.#variants.map(variant => `${this.#unique!.toString()}-${variant.culture}`);
+                    this.#docWorkspaceCtx?.readOnlyGuard.removeRules(ruleKeys);
                 }
 
                 // If previously the page was locked by someone else, we can alert the user its now unlocked
