18764d233e6f23decc77805112488e7e524d9e8f
Warren Buckley <warren@hackmakedo.com>
2025-11-13 17:13:16 +0000

    Uses ContentLockService to check if node is locked
    ContentLockService is Scoped for the DI - so use IServiceScopeFactory

--- ContentLock/FlagProviders/IsLockedFlagProvider.cs
+++ ContentLock/FlagProviders/IsLockedFlagProvider.cs
@@ -1,4 +1,8 @@
-﻿using Umbraco.Cms.Api.Management.Services.Flags;
+﻿using ContentLock.Interfaces;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Umbraco.Cms.Api.Management.Services.Flags;
 using Umbraco.Cms.Api.Management.ViewModels;
 using Umbraco.Cms.Api.Management.ViewModels.Document.Collection;
 using Umbraco.Cms.Api.Management.ViewModels.Document.Item;
@@ -7,7 +11,13 @@ using Umbraco.Cms.Api.Management.ViewModels.Tree;
 namespace ContentLock.FlagProviders;
 public class IsLockedFlagProvider : IFlagProvider
 {
+    public IsLockedFlagProvider(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
     private const string Alias = Umbraco.Cms.Core.Constants.Conventions.Flags.Prefix + "ContentLock.Locked";
+    private readonly IServiceScopeFactory _serviceScopeFactory;
 
     // Indicate that this flag provider only provides flags for documents.
     public bool CanProvideFlags<TItem>()
@@ -19,10 +29,21 @@ public class IsLockedFlagProvider : IFlagProvider
     public async Task PopulateFlagsAsync<TItem>(IEnumerable<TItem> itemViewModels)
         where TItem : IHasFlags
     {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var contentLockService = scope.ServiceProvider.GetRequiredService<IContentLockService>();
+
+        // Get all locks currently in the site - once for performance
+        var allLocks = await contentLockService.GetLockOverviewAsync();
+        var lockedKeys = new HashSet<Guid>(allLocks.Items.Select(x => x.Key));
+
         foreach (TItem item in itemViewModels)
         {
-            // DEMO: Just add it to everything
-            item.AddFlag(Alias);
+            // IHasFlags exposes Id, so we can check it directly
+            // without casting or pattern matching to the diff models
+            if (lockedKeys.Contains(item.Id))
+            {
+                item.AddFlag(Alias);
+            }
         }
     }
 }
