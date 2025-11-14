1e569bb4b6c008447f2c092f9ac5c0223a10a30b
Warren Buckley <warren@hackmakedo.com>
2025-11-14 17:37:43 +0000

    Try running the tests on HTTPS in GH Action run - I feel this was a problem before though

--- ContentLock.Playwright.Tests/playwright.config.ts
+++ ContentLock.Playwright.Tests/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI ? 'http://localhost:5000' : 'https://localhost:5001',
+    baseURL: 'https://localhost:5001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
