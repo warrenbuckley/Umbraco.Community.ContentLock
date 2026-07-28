09922a494cfeae1619217f479964a35950159e9e
Warren Buckley <warren@hackmakedo.com>
2026-07-28 14:05:09 +0100

    feat: migrate OpenAPI document registration to Microsoft.AspNetCore.OpenApi
    Umbraco 18 replaces Swashbuckle with Microsoft.AspNetCore.OpenApi for OpenAPI
    generation (umbraco/Announcements#32), removing SwaggerGenOptions,
    IOperationIdHandler and BackOfficeSecurityRequirementsOperationFilterBase.
    
    Rewrite the "contentlock" document registration using the new
    AddBackOfficeOpenApiDocument builder and a custom IOpenApiOperationTransformer
    that preserves the existing simple operation IDs (Lock/Unlock/BulkUnlock),
    keeping the generated TypeScript client's method names stable.

--- ContentLock/Composers/ContentLockApiComposer.cs
+++ ContentLock/Composers/ContentLockApiComposer.cs
@@ -1,14 +1,11 @@
-using Asp.Versioning;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Api.Management.OpenApi;
 using Umbraco.Cms.Api.Common.OpenApi;
+using Umbraco.Cms.Api.Management.OpenApi;
 using ContentLock.Interfaces;
 using ContentLock.Services;
 
@@ -21,59 +18,53 @@ namespace ContentLock.Composers
             // Service
             builder.Services.AddScoped<IContentLockService, ContentLockService>();
 
-            // Swagger Operation Handler to generate the OpenAPI Spec nicer
-            builder.Services.AddSingleton<IOperationIdHandler, CustomOperationHandler>();
+            // Related documentation:
+            // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api
+            // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/adding-a-custom-swagger-document
+            // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/versioning-your-api
+            // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/access-policies
 
-            // Configure SwaggerGen (OpenAPI Doc)
-            builder.Services.Configure<SwaggerGenOptions>(opt =>
-            {
-                // Related documentation:
-                // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api
-                // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/adding-a-custom-swagger-document
-                // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/versioning-your-api
-                // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/access-policies
-
-                // Configure the Swagger generation options
-                // Add in a new Swagger API document solely for our own package that can be browsed via Swagger UI
-                // Along with having a generated swagger JSON file that we can use to auto generate a TypeScript client
-                opt.SwaggerDoc(Constants.ApiName, new OpenApiInfo
-                {
-                    Title = "Content Lock Backoffice API",
-                    Version = "1.0",
-                    Contact = new OpenApiContact
+            // Add a new OpenAPI document solely for our own package that can be browsed via the OpenAPI UI
+            // Along with having a generated openapi JSON file that we can use to auto generate a TypeScript client
+            builder.AddBackOfficeOpenApiDocument(
+                Constants.ApiName,
+                document => document
+                    .WithTitle("Content Lock Backoffice API")
+                    .WithBackOfficeAuthentication()
+                    .WithJsonOptions(Umbraco.Cms.Core.Constants.JsonOptionsNames.BackOffice)
+                    .ConfigureOpenApiOptions(options =>
                     {
-                        Name = "Warren Buckley",
-                        Email = "warren@hackmakedo.com",
-                        Url = new Uri("https://hackmakedo.com")
-                    }
-                });
+                        options.AddDocumentTransformer((doc, _, _) =>
+                        {
+                            doc.Info.Version = "1.0";
+                            doc.Info.Contact = new OpenApiContact
+                            {
+                                Name = "Warren Buckley",
+                                Email = "warren@hackmakedo.com",
+                                Url = new Uri("https://hackmakedo.com")
+                            };
+                            return Task.CompletedTask;
+                        });
 
-                // Enable Umbraco authentication for the "Example" Swagger document
-                // PR: https://github.com/umbraco/Umbraco-CMS/pull/15699
-                opt.OperationFilter<ContentLockOperationSecurityFilter>();
-            });
+                        // Generate nice operation IDs in our openapi json file
+                        // So that the generated TypeScript client has nice method names and not too verbose
+                        // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/umbraco-schema-and-operation-ids#operation-ids
+                        options.AddOperationTransformer<ContentLockOperationIdTransformer>();
+                    }));
         }
 
-        public class ContentLockOperationSecurityFilter : BackOfficeSecurityRequirementsOperationFilterBase
+        public class ContentLockOperationIdTransformer : IOpenApiOperationTransformer
         {
-            protected override string ApiName => Constants.ApiName;
-        }
-
-        // This is used to generate nice operation IDs in our swagger json file
-        // So that the generated TypeScript client has nice method names and not too verbose
-        // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/umbraco-schema-and-operation-ids#operation-ids
-        public class CustomOperationHandler : OperationIdHandler
-        {
-            public CustomOperationHandler(IOptions<ApiVersioningOptions> apiVersioningOptions) : base(apiVersioningOptions)
+            public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
             {
-            }
+                if (context.Description.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor &&
+                    controllerActionDescriptor.ControllerTypeInfo.Namespace?.StartsWith("ContentLock.Controllers", StringComparison.InvariantCultureIgnoreCase) is true)
+                {
+                    operation.OperationId = $"{controllerActionDescriptor.RouteValues["action"]}";
+                }
 
-            protected override bool CanHandle(ApiDescription apiDescription, ControllerActionDescriptor controllerActionDescriptor)
-            {
-                return controllerActionDescriptor.ControllerTypeInfo.Namespace?.StartsWith("ContentLock.Controllers", comparisonType: StringComparison.InvariantCultureIgnoreCase) is true;
+                return Task.CompletedTask;
             }
-
-            public override string Handle(ApiDescription apiDescription) => $"{apiDescription.ActionDescriptor.RouteValues["action"]}";
         }
     }
 }
